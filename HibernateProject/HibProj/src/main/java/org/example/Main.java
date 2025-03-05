52f6afa76ab2c591469cba5283e1852c5cd81a85
Nidhi Rajvir <rajvirnidhi@gmail.com>
2025-03-05 19:00:42 +0530

    Refactoring the code

--- HibernateProject/HibProj/src/main/java/org/example/Main.java
+++ HibernateProject/HibProj/src/main/java/org/example/Main.java
@@ -10,28 +10,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Student s = new Student();
-        s.setRollno(23);
-        s.setName("Kirit");
-        s.setAge(64);
+        s.setRollno(26);
+        s.setName("Purva");
+        s.setAge(28);
 
         // 2 ways
         // 1. using xml --> all the resources and xml files go to resources folder
         // 2. using annotations
-        Configuration cfg = new Configuration(); // --> it know it has to find configuration somewhere
-        cfg.addAnnotatedClass(org.example.Student.class); // --> we need to map the class
-        cfg.configure(); // --> this loads the xml for xml you can use default name or can specify the name here
+//        Configuration cfg = new Configuration(); // --> it know it has to find configuration somewhere
+//        cfg.addAnnotatedClass(org.example.Student.class); // --> we need to map the class
+//        cfg.configure(); // --> this loads the xml for xml you can use default name or can specify the name here
+
         // session factory has method open session to return session object
         // session factory is also an interface
         // so buildSessionFactory in configuration gives object of SessionFactory
         //SessionFactory is heavy-weight object so use it only once
-        SessionFactory sf = cfg.buildSessionFactory();
+//        SessionFactory sf = cfg.buildSessionFactory();
+
+
+        //reduce 3 lines of cfg to one
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.example.Student.class)
+                .configure()
+                .buildSessionFactory();
+
         // session is an interface and not class, so we cannot create an object of it
         //NOTE: for any unit of work we will create new session
         Session session = sf.openSession();
 
         Transaction transaction = session.beginTransaction();
-        session.save(s); // this is transaction so we need to commit this transaction
+        session.persist(s);
+
+        // save is deprecated
+        //session.save(s); // this is transaction, so we need to commit this transaction
         transaction.commit();
 
+        //good practice to close it
+        session.close();
+        sf.close();
     }
 }
