627664853cfa2ad30cf4abf6a5b0ce17ed2979f7
Nidhi Rajvir <rajvirnidhi@gmail.com>
2025-04-19 19:40:59 +0530

    fetching value of multiple columns

--- HibernateProject/HQL/src/main/java/org/example/Main.java
+++ HibernateProject/HQL/src/main/java/org/example/Main.java
@@ -23,12 +23,16 @@ public class Main {
         //from Laptop where brand like 'HP'  --> here remember to give single quotes while passing hardcoded value
 
         String brand = "HP";
-        Query query = session.createQuery("select model from Laptop where brand like ?1");
+        Query query = session.createQuery("select brand, model from Laptop where brand like ?1");
         query.setParameter(1,brand);
-        // need to change the type of list as we are fetching model only
-        List<String> laptop_model = query.getResultList();
+        // need to change the type of list as we are fetching 2 columns now
+        List<Object[]> laptops = query.getResultList();
 
-        System.out.println(laptop_model);
+        // getting one row of object array and displaying the values
+        for (Object[] data : laptops)
+        {   // data[0] is by default object type so you need to typecast it
+            System.out.println((String)data[0]+ " " +(String)data[1]);
+        }
 
         session.close();
         sf.close();
